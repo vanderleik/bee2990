097cad53d6fd981c22d3ab2b086aef21711f541c
vanderleik <vanderleik@yahoo.com.br>
2025-03-24 20:07:45 -0300

    initial commit

---
+++ src/main/java/com/bee2990/api/Bee2990Application.java
@@ -0,0 +1,13 @@
+package com.bee2990.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bee2990Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bee2990Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bee2990/api/Bee2990ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bee2990.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bee2990ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
